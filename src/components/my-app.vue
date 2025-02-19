7611417f5a044efd052cd0f36079702aa10ce84d
George Wu <george.wu@rmit.edu.au>
2025-02-19 13:31:51 +1100

    connect to the mqtt

--- src/components/my-app.vue
+++ src/components/my-app.vue
@@ -10,9 +10,14 @@
                     <v-col class="text-center">
                         <span>{{ message }}</span>
                         <br />
-                        <v-btn class="ma-2" variant="text" icon color="pink-lighten-1" @click="click">
+                        <v-btn class="ma-2" variant="text" icon color="pink-lighten-1" @click="handleHeartClick">
                             <v-icon>mdi-heart</v-icon>
                         </v-btn>
+                        <br />
+                        <v-btn class="ma-2" color="primary" @click="connectMQTT">
+                            Connect to MQTT
+                        </v-btn>
+                        <div v-if="mqttStatus">Connection Status: {{ mqttStatus }}</div>
                     </v-col>
                 </v-row>
             </v-container>
@@ -27,6 +32,7 @@
 import { widget } from "@widget-lab/3ddashboard-utils";
 import { mapStores } from "pinia";
 import { useGlobalStore } from "@/store/global";
+import mqtt from 'mqtt';
 
 console.debug(widget);
 
@@ -34,16 +40,64 @@ export default {
     name: "App",
     data() {
         return {
-            message: "hello"
+            message: "hello",
+            mqttClient: null,
+            mqttStatus: null,
+            clickCount: 0
         };
     },
     computed: {
         ...mapStores(useGlobalStore)
     },
     methods: {
-        click() {
-            this.message = "hello from widget";
-            this.globalStore.setTitle("Widget");
+        handleHeartClick() {
+            this.clickCount++;
+            this.message = `Clicked ${this.clickCount} times!`;
+            
+            if (this.mqttClient && this.mqttClient.connected) {
+                this.mqttClient.publish('george/sensor', JSON.stringify({
+                    action: 'heart_click',
+                    count: this.clickCount,
+                    timestamp: new Date().toISOString()
+                }));
+            } else {
+                console.warn('MQTT not connected. Cannot publish message.');
+                this.mqttStatus = 'Not connected to MQTT';
+            }
+        },
+        connectMQTT() {
+            if (this.mqttClient) {
+                this.mqttClient.end();
+            }
+
+            const options = {
+                protocol: 'wss',
+                hostname: 'test.mosquitto.org',
+                port: 8081,
+                clientId: 'vue-client-' + Math.random().toString(16).substr(2, 8)
+            };
+
+            this.mqttStatus = 'Connecting...';
+            this.mqttClient = mqtt.connect(options);
+
+            this.mqttClient.on('connect', () => {
+                console.log('Connected to MQTT broker');
+                this.mqttStatus = 'Connected';
+                this.mqttClient.subscribe('george/sensor', (err) => {
+                    if (!err) {
+                        console.log('Subscribed to george/sensor');
+                    }
+                });
+            });
+
+            this.mqttClient.on('message', (topic, message) => {
+                console.log(`Received message from ${topic}: ${message.toString()}`);
+            });
+
+            this.mqttClient.on('error', (error) => {
+                console.error('MQTT Error:', error);
+                this.mqttStatus = 'Error: ' + error.message;
+            });
         }
     }
 };
